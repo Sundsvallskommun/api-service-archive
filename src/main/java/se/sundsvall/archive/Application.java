6e1d9450afdce9c83f223e9060890046dcda19f5
Jonas Klingstedt <jonas.klingstedt@gmail.com>
2022-02-25 14:39:43 +0100

    UF-XYZ - Docker-related changes
    * Changes some minor Docker-related things

--- src/main/java/se/sundsvall/archive/Application.java
+++ src/main/java/se/sundsvall/archive/Application.java
@@ -1,6 +1,10 @@
 package se.sundsvall.archive;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 
 import se.sundsvall.dept44.ServiceApplication;
 
@@ -9,5 +13,13 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	CommandLineRunner startupDebug(
+			@Value("${integration.formpipe-proxy.base-url}") final String formpipeProxyIntegrationBaseUrl) {
+		return args -> {
+			LoggerFactory.getLogger("STARTUP").info("Formpipe Proxy base URL = '{}'", formpipeProxyIntegrationBaseUrl);
+		};
 	}
 }
