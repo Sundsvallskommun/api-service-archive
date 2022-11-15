592cb3893ea8a09b2db1743a32788d2d1fb5ec6e
Jonas Klingstedt <jonas.klingstedt@gmail.com>
2022-11-15 14:04:54 +0100

    Replaces the RestTemplate client with Feign

---
+++ src/main/java/se/sundsvall/archive/integration/formpipeproxy/FormpipeProxyClient.java
@@ -0,0 +1,19 @@
+package se.sundsvall.archive.integration.formpipeproxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import se.sundsvall.archive.integration.formpipeproxy.domain.ImportRequest;
+import se.sundsvall.archive.integration.formpipeproxy.domain.ImportResponse;
+
+@FeignClient(
+    name = FormpipeProxyIntegration.INTEGRATION_NAME,
+    url = "${integration.formpipe-proxy.base-url}",
+    configuration = FormpipeProxyIntegrationConfiguration.class
+)
+interface FormpipeProxyClient {
+
+    @PostMapping("/api/import")
+    ResponseEntity<ImportResponse> postImport(ImportRequest request);
+}
